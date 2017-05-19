16eb146b2a9dcd52e427443c35bcf7a4a03ad4f2
szeidman <sam.zeidman@gmail.com>
2017-05-19 11:53:19 -0400

    populate Blocker class instances from scrape

--- lib/blockers/blocker.rb
+++ lib/blockers/blocker.rb
@@ -4,24 +4,33 @@ require 'open-uri'
 
 class Blocker
 
-  attr_accessor :name :url :price
+  attr_accessor :name, :url, :price
 
   @@all = []
 
-  def initialize
+  def self.all
+    @@all
+  end
+
+  def initialize(name, url, price)
+    @name = name
+    @url = url
+    @price = price
+    @@all << self
   end
 
 
-  def self.scraper_main
+  def self.scrape
     doc = Nokogiri::HTML(open('http://www.goaliemonkey.com/equipment/blockers/sr-goalie-blockers.html'))
     doc.css("li.item.span3").each do |item|
-      product = self.new
-      product.name = item.css("div.caption h2.product-name a").text
-      product.price = item.css("span.regular-price span.price").text
-      product.product_url = item("div.caption h2.product-name a").attribute("href").value
+      name = item.css("div.caption h2.product-name a").text
+      url = item.css("div.caption h2.product-name a").attribute("href").value
+      price = item.css("span.regular-price span.price").text
+      self.new(name, url, price)
+      binding.pry
     end
   end
 
-
-
 end
+
+Blocker.scrape
