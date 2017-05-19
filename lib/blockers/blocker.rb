318a88a63f807f9e4f6edb84d2ec3c8c7d81cbad
szeidman <sam.zeidman@gmail.com>
2017-05-18 20:12:21 -0400

    initial code for scrapes

--- lib/blockers/blocker.rb
+++ lib/blockers/blocker.rb
@@ -1,1 +1,3 @@
 class Blockers::Blocker
+
+end
--- lib/blockers/scraper.rb
+++ lib/blockers/scraper.rb
@@ -1,3 +1,16 @@
 Class Blockers::Scraper
 
+  def self.scrape_blocker_type_page
+    categories = Nokogiri::HTML(open('http://www.goaliemonkey.com/equipment/blockers.html'))
+    # Blocker Category: (div.category-thums.span3 a p.name).text
+    # Blocker Category URL: (div.category-thums.span3 a).attribute("href").value
+    # Category URL should return as category_url
+  end
+
+  def self.scrape_blockers_of_type_page(category_url)
+    # Add show/all.html to category_url
+    selection = Nokogiri::HTML(open('http://www.goaliemonkey.com/equipment/blockers.html'))
+    #Blocker name
+    #Blocker price
+    #Blocker URL
 end
