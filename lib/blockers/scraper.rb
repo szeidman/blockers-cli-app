3825f765490af9875ffdc45b10edded4682eff63
szeidman <sam.zeidman@gmail.com>
2017-05-18 20:20:54 -0400

    refine scraping comments to code in later

--- lib/blockers/scraper.rb
+++ lib/blockers/scraper.rb
@@ -2,6 +2,7 @@ Class Blockers::Scraper
 
   def self.scrape_blocker_type_page
     categories = Nokogiri::HTML(open('http://www.goaliemonkey.com/equipment/blockers.html'))
+    # iterate over div.category-thums.span3
     # Blocker Category: (div.category-thums.span3 a p.name).text
     # Blocker Category URL: (div.category-thums.span3 a).attribute("href").value
     # Category URL should return as category_url
@@ -10,7 +11,8 @@ Class Blockers::Scraper
   def self.scrape_blockers_of_type_page(category_url)
     # Add show/all.html to category_url
     selection = Nokogiri::HTML(open('http://www.goaliemonkey.com/equipment/blockers.html'))
-    #Blocker name
-    #Blocker price
-    #Blocker URL
+    # iterate over the li.item.span3 (note that there is a li.first.item out there...)
+    #Blocker name   div.caption h2.product-name a .text
+    #Blocker price   div.price-line
+    #Blocker URL   div.caption h2.product-name a 'href' .value
 end
